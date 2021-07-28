8c9dd11095f714c753ded866327dea6f6d4d4807
omar-ghannou <omar_ghannou@live.fr>
2021-07-28 22:33:59 +0100

    adding premake file to generate the project for multiple Platforms/IDEs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GEngine
 Game Engine!
+
+<b>VS2019</b>
+if you are generating the project for visual studio, DO NOT forget to mark the sandbox as the startup project.
---
+++ premake5.lua
@@ -0,0 +1,102 @@
+workspace "GEngine"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GEngine"
+	location "GEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
+	
+	files{
+
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+
+	}
+
+	includedirs {
+		"%{prj.name}/vendors/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines{
+			"GE_PLATFORM_WINDOWS",
+			"GE_BUILD_DLL",
+			"_WINDLL"
+		}
+
+		postbuildcommands{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
+	
+	files{
+
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+
+	}
+
+	includedirs {
+		"GEngine/vendors/spdlog/include",
+		"GEngine/src"
+	}
+
+	links{
+		"GEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines{
+			"GE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GE_DIST"
+		optimize "On"
